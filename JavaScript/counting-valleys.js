69802470709c707602dd162e2c7d39f3420d4ab5
Garrett <garrettpyke@msn.com>
2022-04-28 19:32:22 -0600

    Add counting-valleys algorithm, edit comments in palindrome

---
+++ JavaScript/counting-valleys.js
@@ -0,0 +1,43 @@
+///// Counting Valleys /////
+/*
+A string input describes a hiking journey in steps up or steps down. The challenge: find the number of valleys hiked.
+
+sample input: 
+8 (integer)
+UDDDUDUU (string)
+
+Visually this looks like:
+_/\      _ 
+   \    /
+    \/\/
+
+This would be considered 1 valley. A valley is defined as any number of steps below sea level followed by any number of steps back to sea level. Hikes always begin and end at sea level.
+*/
+
+// If we represent the position above/below sea level as an integer based on steps made, whenever the integer *increases* to 0 a valley was just "passed".
+//  We could test for mountains and valleys with the same logic if needed.
+
+const log = (arg) => console.log(arg) //just a shortcut for console.logs
+
+testSteps = 8;
+testTrip = 'UDDDUDUU'
+
+valleyCount = (steps, trip) => {
+    valleys = 0;
+    position = 0;
+    for(i=0; i < steps; i++) {
+        if (trip.at(i) === "D") {
+            position--;
+        }
+        else if (trip.at(i) === "U") {
+            position ++;
+            if (position === 0) {
+                valleys++;
+            }
+        }
+        log(`Pass: ${i}, valleys: ${valleys}, position: ${position}`)
+    }
+    return `Number of valleys: ${valleys}`
+}
+
+log(valleyCount(testSteps, testTrip))
--- JavaScript/palindrome.js
+++ JavaScript/palindrome.js
@@ -1,13 +1,13 @@
-///// Palindrome test /////
-// Set 'str' variable to any word; this function will return true or false.
+///// Palindrome Test /////
+// Set 'str' variable to any word; this function will return true if word is a palindrome.
 
-// Examples: kayak, level, civic, madam, mom, noon, racecar, saippuakivikauppias, sexatnoontaxes - all return true
+// Examples: kayak, level, civic, madam, mom, noon, racecar, saippuakivikauppias, sexatnoontaxes - all return true.
 
 // This function works from the outside in. Thus if a near-palindrome has faults at the edges, it will execute in relatively few cycles. If the faults are near the center, it's not as optimized.
 
 //get str length
 //compare outer chars
-//increment inwards until midpoint, which is str.len / 2 if even
+//increment inwards until midpoint, which is str.len / 2
 
 str = 'kayak'
 console.log('str.at(0): ', str.at(0)) // testing...output: k for kayak input
@@ -16,8 +16,8 @@ console.log('str.at(4): ', str.at(4)) // testing...output: k
 len = str.length
 
 function palindromeCheck(word) { 
-    j = 1 //keeps track of end characters  
-    // stop testing at midpoint of word. len/2 works for both odd and even numbered letters b/c modulus is dropped.
+    j = 1 // Keep track of end characters  
+    // Stop testing at midpoint of word. len/2 works for both odd and even numbered palindromes (modulus is dropped).
     for (i=0; i < len/2; i++) {
         if (word.at(i) !== word.at(len-j)) {
             return false
