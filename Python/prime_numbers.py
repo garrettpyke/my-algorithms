30e73bcb707e69d89163d3c311d0bff07dd07b0e
garrettpyke <garrettpyke@msn.com>
2025-04-04 15:37:13 -0600

    Make prime_numbers pseudocode more explicit & begin refactor

--- Python/prime_numbers.py
+++ Python/prime_numbers.py
@@ -1,19 +1,33 @@
+""" - Iteration 1 -
+def prime(number)
+    if number is 1
+        return '{number} is prime'
+    for divisor in range(2, number)
+        if number % divisor == 0
+            number is not prime, move to next lowest
+            return prime(number - 1)
+        continue in the range up to number - 1
+    all divisors have been checked, print '{number} is prime'
+    move to next lowest, return prime(number - 1)    
 """
-def prime(num)
-    if num = 1
-        return 'num is prime'
-    for divisor in range(2, num)
-        if num % divisor == 0
-            return prime(num - 1)
-        continue
-    print 'num is prime'
-    return prime(num - 1)    
+"""  - Iteration 2 -
+def prime(number)
+    if number is 1
+        return '{number} is prime'
+    for divisor in range(2, number)
+        if number % divisor == 0
+            number is not prime, move to next lowest
+            #todo: number-- & ?
+        continue in the range up to number - 1
+    all divisors have been checked, print '{number} is prime'
+    move to next lowest, return prime(number - 1)    
 """
 
 def prime_numbers(num):
     """
-    Recursive prime numbers finder. In this iteration, it's not very elegant or 
-    condensed.
+    Recursive prime numbers finder. In this iteration it's not very elegant or 
+    condensed, so consider how it can be refactored to fewer lines, starting with
+    the return statements.
     
     Args:
         number (int): The input number. Note: Max recursion error
@@ -32,4 +46,4 @@ def prime_numbers(num):
     print(f"{num} is prime.")
     return prime_numbers(num - 1)
         
-print(prime_numbers(30))    
+print(prime_numbers(502))    
