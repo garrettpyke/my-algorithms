98e59b4a03cbd5fed3f20f97bf9ba723e412abb0
garrettpyke <garrettpyke@msn.com>
2025-04-03 19:23:07 -0600

    Add recursive factorial function to Python

---
+++ Python/factorial.py
@@ -0,0 +1,19 @@
+number = 5
+
+def factorial(number):
+    """
+    Recursive factorial function.
+    
+    Args:
+        number (int): The input number. Note: Max recursion error
+        possible, depending on hardware and Python version.
+
+    Returns:
+        int: The result of number!
+    """
+    
+    if number == 0:
+        return 1
+    return number * factorial(number - 1) 
+
+print(f"{number}! = {factorial(number)}")
